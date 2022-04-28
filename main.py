55c136bd2243179f802a6643d16daf2430715a70
Constantin Philippenko <constantin.philippenko@polytechnique.edu>
2022-04-28 15:56:44 +0200

    main.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ NB_CLIENTS = 10
 DATASET_NAME = "mnist"
 
 NB_LABELS = 10
-NB_RUNS = 2
+NB_RUNS = 6
 
 
 if __name__ == '__main__':
@@ -29,21 +29,21 @@ if __name__ == '__main__':
         my_metrics.set_metrics_on_X(EM_distance_on_X)
 
         ########## Compute metrics on Y ##########
-        # KL_distance_on_Y, TV_distance_on_Y = compute_metrics_on_Y(iid_clients_network, non_iid_clients_network)
-        # my_metrics.set_metrics_on_Y(KL_distance_on_Y, TV_distance_on_Y)
+        KL_distance_on_Y, TV_distance_on_Y = compute_metrics_on_Y(iid_clients_network, non_iid_clients_network)
+        my_metrics.set_metrics_on_Y(KL_distance_on_Y, TV_distance_on_Y)
 
         # ########## Compute metrics on Y | X ##########
-        # KL_distance_on_Y_given_X, TV_distance_on_Y_given_X = compute_metrics_on_Y_given_X(iid_clients_network,
-        #                                                                                   non_iid_clients_network)
-        # my_metrics.set_metrics_on_Y_given_X(KL_distance_on_Y_given_X, TV_distance_on_Y_given_X)
+        KL_distance_on_Y_given_X, TV_distance_on_Y_given_X = compute_metrics_on_Y_given_X(iid_clients_network,
+                                                                                          non_iid_clients_network)
+        my_metrics.set_metrics_on_Y_given_X(KL_distance_on_Y_given_X, TV_distance_on_Y_given_X)
 
         ########## Compute metrics on X | Y ##########
-        # EM_distance_on_X_given_Y = compute_metrics_on_X_given_Y(iid_clients_network, non_iid_clients_network)
-        # my_metrics.set_metrics_on_X_given_Y(EM_distance_on_X_given_Y)
+        EM_distance_on_X_given_Y = compute_metrics_on_X_given_Y(iid_clients_network, non_iid_clients_network)
+        my_metrics.set_metrics_on_X_given_Y(EM_distance_on_X_given_Y)
 
     my_metrics.save_itself()
 
-    # my_metrics.plot_Y_metrics()
+    my_metrics.plot_Y_metrics()
     my_metrics.plot_X_metrics()
-    # my_metrics.plot_X_given_Y_metrics()
-    # my_metrics.plot_Y_given_X_metrics()
+    my_metrics.plot_X_given_Y_metrics()
+    my_metrics.plot_Y_given_X_metrics()
