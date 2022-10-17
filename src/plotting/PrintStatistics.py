b856265162e3b9f2b109b67da31258e3f7640c64
Constantin Philippenko <constantin.philippenko@polytechnique.edu>
2022-10-17 16:09:11 +0200

    Remove useless files.

--- src/plotting/PrintStatistics.py
+++ src/plotting/PrintStatistics.py
@@ -35,6 +35,46 @@ def print_latex_table(dataset_names):
     column1 = np.array([["X mean"], ["X max"], ["Y mean"], ["Y max"], ["Entropy"]])
     table = np.append(column1, table, axis=1)
     print(tabulate(table, tablefmt="latex", floatfmt=".2f"))
+    plot_metrics(X_max, X_mean, Y_max, Y_mean, entropy, datasets_name)
+
+
+def plot_metrics(X_max, X_mean, Y_max, Y_mean, entropy, datasets_name):
+    width = 0.25
+    fontsize = 12
+
+    fig, ax1 = plt.subplots()
+
+    ind = np.arange(len(datasets_name))
+
+    greens = plt.cm.Greens
+    reds = plt.cm.Reds
+    blues = plt.cm.Blues
+
+    xmax = ax1.bar(ind, X_max, width, label="X max", lw=2, color=blues(0.4))
+    xmean = ax1.bar(ind, X_mean, width, label="X mean", lw=2, color=blues(0.8))
+
+
+    ymax = ax1.bar(ind + width, Y_max, width, label="Y max", lw=2, color=reds(0.4))
+    ymean = ax1.bar(ind + width, Y_mean, width, label="Y mean", lw=2, color=reds(0.8))
+
+    ax2 = ax1.twinx()
+    ent = ax2.bar(ind + 2*width, entropy, width, label="Entropy", lw=2, color=greens(0.6))
+
+    ax1.tick_params(axis='x', labelsize=fontsize)
+    ax1.tick_params(axis='y', labelsize=fontsize)
+    ax2.tick_params(axis='y', labelcolor=greens(0.9), labelsize=fontsize)
+
+    ax1.set_ylim([0, np.max(np.concatenate([X_max[1:], X_mean[1:], Y_max, Y_mean]))])
+
+    fig.tight_layout()
+
+    legends = [xmax, xmean, ymax, ymean, ent]
+    labels = [l.get_label() for l in legends]
+    plt.legend(legends, labels, loc="best", fontsize=fontsize)
+
+    plt.xticks(ind + width / 2, datasets_name)
+
+    plt.savefig('{0}/{1}.pdf'.format("{0}/pictures".format(get_project_root()), "comparison"), format='pdf', bbox_inches='tight')
 
 
 def print_pvalue(dataset_names):
